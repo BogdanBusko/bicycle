a707e1cb91bb19cef7997bb3d23d2e9579336d75
BogdanBusko <busko.bogdan@gmail.com>
2017-07-13 17:16:29 +0300

    generated models bicycle and bicycle_category and configurated db

---
+++ db/migrate/20170713141214_create_bicycle_categories.rb
@@ -0,0 +1,9 @@
+class CreateBicycleCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bicycle_categories do |t|
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
